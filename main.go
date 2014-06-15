0a0b8d3b265d7185e95bccc801892ca4025c2ed6
Shinji Tanaka <shinji.tanaka@gmail.com>
2014-06-15 21:14:37 +0900

    apply go fmt

--- main.go
+++ main.go
@@ -1,84 +1,84 @@
 package main
 
 import (
-  "github.com/BurntSushi/toml"
-  "flag"
-  "log"
-  "net/http"
-  "fmt"
-  "io/ioutil"
-  "bytes"
-  "strconv"
-  //"html"
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	//"html"
 )
 
 type tomlConfig struct {
-  Port int
-  Relay relayInfo
+	Port  int
+	Relay relayInfo
 }
 
 type relayInfo struct {
-  Name string
-  Type string
-  EndPoint string
+	Name     string
+	Type     string
+	EndPoint string
 }
 
 var config tomlConfig
 
 func topHandler(w http.ResponseWriter, r *http.Request) {
-  log.Printf("%s", r.URL.Path)
-  body, err := ioutil.ReadAll(r.Body);
-  if err != nil {
-    log.Printf("Err: %s", err)
-  }
+	log.Printf("%s", r.URL.Path)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Err: %s", err)
+	}
 
-  for h := range r.Header {
-    for v := range r.Header[h] {
-      log.Printf("Header: %s: %s", h, r.Header[h][v])
-    }
-  }
-  log.Printf("Body: %s", body)
-  log.Printf("EndPoint: %s", config.Relay.EndPoint)
-  log.Printf("Method: %s", r.Method)
+	for h := range r.Header {
+		for v := range r.Header[h] {
+			log.Printf("Header: %s: %s", h, r.Header[h][v])
+		}
+	}
+	log.Printf("Body: %s", body)
+	log.Printf("EndPoint: %s", config.Relay.EndPoint)
+	log.Printf("Method: %s", r.Method)
 
-  fmt.Fprintf(w, "OK\n")
-  if config.Relay.EndPoint == "" {
-    return
-  }
+	fmt.Fprintf(w, "OK\n")
+	if config.Relay.EndPoint == "" {
+		return
+	}
 
-  req, err := http.NewRequest(r.Method,
-    config.Relay.EndPoint, bytes.NewReader(body))
-  if err != nil {
-    log.Printf("Err: %s", err)
-  }
-  req.Header = r.Header
-  client := &http.Client{}
-  resp, err := client.Do(req)
-  if err != nil {
-    log.Printf("Err: %s", err)
-  }
-  defer resp.Body.Close()
-  relay_body, err := ioutil.ReadAll(resp.Body)
-  log.Printf("Relay Body: %s", relay_body)
+	req, err := http.NewRequest(r.Method,
+		config.Relay.EndPoint, bytes.NewReader(body))
+	if err != nil {
+		log.Printf("Err: %s", err)
+	}
+	req.Header = r.Header
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Err: %s", err)
+	}
+	defer resp.Body.Close()
+	relay_body, err := ioutil.ReadAll(resp.Body)
+	log.Printf("Relay Body: %s", relay_body)
 }
 
 func main() {
-  var conffile = flag.String("conf", "", "Configuration File")
-  flag.Parse()
+	var conffile = flag.String("conf", "", "Configuration File")
+	flag.Parse()
 
-  log.Printf("Config file: %s", *conffile)
+	log.Printf("Config file: %s", *conffile)
 
-  if *conffile != "" {
-    if _, err := toml.DecodeFile(*conffile, &config); err != nil {
-      panic(err)
-    }
-  }
+	if *conffile != "" {
+		if _, err := toml.DecodeFile(*conffile, &config); err != nil {
+			panic(err)
+		}
+	}
 
-  port := config.Port
-  if port == 0 {
-    port = 18080
-  }
-  http.HandleFunc("/", topHandler)
-  http.ListenAndServe(":" + strconv.Itoa(port), nil)
-  log.Print("Shutting down..")
+	port := config.Port
+	if port == 0 {
+		port = 18080
+	}
+	http.HandleFunc("/", topHandler)
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	log.Print("Shutting down..")
 }
--- main_test.go
+++ main_test.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  "testing"
-  "net/http"
-  "net/http/httptest"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestHandler(t *testing.T) {
-  ts := httptest.NewServer( http.HandlerFunc( topHandler ) )
-  defer ts.Close()
+	ts := httptest.NewServer(http.HandlerFunc(topHandler))
+	defer ts.Close()
 
-  res, err := http.Get( ts.URL + "/test" )
-  if err != nil {
-    t.Error("unexpected")
-    return
-  }
+	res, err := http.Get(ts.URL + "/test")
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
 
-  if res.StatusCode != 200 {
-    t.Error("Status code error")
-    return
-  }
+	if res.StatusCode != 200 {
+		t.Error("Status code error")
+		return
+	}
 }
